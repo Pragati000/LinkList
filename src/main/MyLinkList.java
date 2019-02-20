c280d1c19b53658926f90e647b3c37cd6ec0ef5e
Pragati000 <pragati.goswami_ccv172gla.ac.in>
2019-02-19 21:58:58 -0800

    add getter and setter for head ,tail,currentsize

--- src/main/MyLinkList.java
+++ src/main/MyLinkList.java
@@ -4,5 +4,30 @@ public class MyLinkList {
     private Node head;
     private Node tail;
     private int currentsize;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public int getCurrentsize() {
+        return currentsize;
+    }
+
+    public void setCurrentsize(int currentsize) {
+        this.currentsize = currentsize;
+    }
+
 }
 
